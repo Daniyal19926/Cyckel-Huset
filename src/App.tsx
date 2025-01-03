609849b43ad6676a7454da75b153025c3a337e99
Daniyal <you@example.com>
2025-01-03 16:02:31 +0100

    added productList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import ProductList from "./components/ProductList";
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Cyckel Huset <i className="fas fa-heart" />
-      </h1>
+    <div className="p-8 ">
+      <div className="mt-40 flex flex-row">
+        <div className=" basis-1/5"></div>
+        <div className="  basis-4/5">
+          <p className=" ms-2 text-xl font-bold grid  ">All Products</p>
+          <ProductList />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,12 @@
+export default function ProductList() {
+  return (
+    <div className="mt-2 grid grid-cols-3 gap-4">
+      <div className="border  w-full h-56"></div>
+      <div className="border  w-full h-56"></div>
+      <div className="border  w-full h-56"></div>
+      <div className="border  w-full h-56"></div>
+      <div className="border  w-full h-56"></div>
+      <div className="border  w-full h-56"></div>
+    </div>
+  );
+}
