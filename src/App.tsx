1d100a42f01c90a999c866aee1fedaf6aad81524
Daniyal <you@example.com>
2025-01-03 09:54:38 +0100

    configured tailwind and fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
 export default function App() {
-  return <h1>Cyckel Huset</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Cyckel Huset <i className="fas fa-heart" />
+      </h1>
+    </div>
+  );
 }
